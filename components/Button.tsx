0a38b9765d75279ee5554089347b6df6f6957b74
LCSF2423 <c0945951@mylambton.ca>
2025-09-06 10:48:53 -0400

    Bug Fix

--- components/Button.tsx
+++ components/Button.tsx
@@ -3,12 +3,13 @@ type ButtonProps = {
   type: "button" | "submit" | "reset";
   title: string;
   icon?: string;
-  variant: string
+  variant: string;
+  full?: boolean;
 }
 
-const Button = ({ type, title, icon, variant }: ButtonProps) => {
+const Button = ({ type, title, icon, variant, full }: ButtonProps) => {
   return (
-    <button className={`flexCenter gap-3 rounded-full border ${variant}`}
+    <button className={`flexCenter gap-3 rounded-full border ${variant} ${full && 'w-full'}`}
       type={type}>
       {icon && <img src={icon} alt={title} width={24} height={24}></img> }
       <label className="bold-16 whitespace-nowrap">{title}</label>
