ea229466800dbdbbe41bafb1bb6d936255d08bb6
LCSF2423 <c0945951@mylambton.ca>
2026-06-12 21:51:07 -0400

    Retest

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,14 +1,14 @@
 import { test, expect } from '@playwright/test';
 
 test('Login', async ({ page }) => {
-  await page.goto('http://localhost:3000/login');
+  await page.goto('https://adventure-react-next-tailwind.vercel.app/login');
     await page.getByRole('textbox', { name: 'Email' }).click();
     await page.getByRole('textbox', { name: 'Email' }).fill('123@gmail.com');
     await page.getByRole('textbox', { name: 'Password' }).click();
     await page.getByRole('textbox', { name: 'Password' }).fill('123456');
     await page.getByRole('button', { name: 'Sign In' }).click();
 
-  await expect(page.getByRole('heading', {name: 'AdventureHub Outdoor Center'})).toBeVisible();
+    await expect(page.getByRole('heading', {name: 'AdventureHub Outdoor Center'})).toBeVisible();
 });
 
 
